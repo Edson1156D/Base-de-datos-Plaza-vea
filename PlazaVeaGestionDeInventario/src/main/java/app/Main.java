e63b4248268a0602f9c23287cb82ec5fac251e1c
ping0lin <joseph_loarte@hotmail.com>
2025-12-04 01:25:32 -0500

    Add files via upload

--- PlazaVeaGestionDeInventario/src/main/java/app/Main.java
+++ PlazaVeaGestionDeInventario/src/main/java/app/Main.java
@@ -1,20 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package app;
 
-/**
- *
- * @author KEVIN
- */
+import vista.ProductosForm; // 
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Esta instrucción inicia la ventana de manera segura
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                // Crea una nueva instancia de tu formulario y la hace visible
+                new ProductosForm().setVisible(true);
+            }
+        });
     }
-    
 }
