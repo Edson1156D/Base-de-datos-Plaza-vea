234e06bd58805a0b91dab425bd6e7878ea251857
ping0lin <joseph_loarte@hotmail.com>
2025-12-04 01:26:47 -0500

    Add files via upload

--- PlazaVeaGestionDeInventario/src/main/java/controlador/ProductoController.java
+++ PlazaVeaGestionDeInventario/src/main/java/controlador/ProductoController.java
@@ -1,13 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controlador;
 
-/**
- *
- * @author KEVIN
- */
+import modelo.Producto;
+import dao.ProductoDAO;
+import dao_impl.ProductoDAOImpl;
+import java.util.List; // <--- Importante
+
 public class ProductoController {
+
+    private ProductoDAO productoDao;
+
+    public ProductoController() {
+        this.productoDao = new ProductoDAOImpl();
+    }
+
+    public boolean guardarProducto(Producto p) {
+        return productoDao.registrarProducto(p);
+    }
     
+    // --- AGREGA ESTE MÉTODO NUEVO ---
+    public List<Producto> listarProductos() {
+        return productoDao.listarProductos();
+    }
+    public boolean actualizarProducto(Producto p) {
+        return productoDao.actualizarProducto(p);
+    }
+
+    public boolean eliminarProducto(int id) {
+        return productoDao.eliminarProducto(id);
+    }
+    public List<Producto> buscarPorNombre(String nombre) {
+        return productoDao.buscarPorNombre(nombre);
+    }
+    
+    public List<Producto> filtrarPorStock() {
+        return productoDao.filtrarPorStock();
+    }
+    
+    public List<Producto> filtrarPorPrecio(double precio) {
+        return productoDao.filtrarPorPrecio(precio);
+    }
+    
+    public List<Producto> filtrarPorTipo(int idTipo) {
+        return productoDao.filtrarPorTipo(idTipo);
+    }
 }
